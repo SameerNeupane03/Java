39bcc328b551764dd8f33fa567717fe45aef2efd
SameerNeupane03 <135142257+SameerNeupane03@users.noreply.github.com>
2024-02-11 17:34:53 +0545

    Add files via upload

---
+++ typecasting.java
@@ -0,0 +1,16 @@
+public class typecasting {
+    public static void main(String args[]){
+    byte evenNum = 80;
+    int typecastEvenNum = (int) evenNum;
+
+    float floatingPoint = 59.88f;
+    int var = (int) floatingPoint;
+
+    char alphabet = 'a';
+    float alphabetValue = (float) alphabet;
+
+    System.out.println(typecastEvenNum);
+    System.out.println(var);
+    System.out.println(alphabetValue);
+
+}}
