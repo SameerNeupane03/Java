8b866f7983124fa64b345208bfebbf51cce45a8a
SameerNeupane03 <135142257+SameerNeupane03@users.noreply.github.com>
2024-02-11 17:35:58 +0545

    Add files via upload

---
+++ commandLineArgumentWithWrapperClass.java
@@ -0,0 +1,9 @@
+public class commandLineArgumentWithWrapperClass {
+    public static void main(String[] args){
+        int multiple = 1;
+        for(int i = 0; i < args.length; i++){
+        multiple = multiple * Integer.parseInt(args[i]);
+        }
+        System.out.println(multiple);
+    }
+}
