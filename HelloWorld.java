fea5be91648be7d6ec70d1e0162f5f5717607a4d
SameerNeupane03 <135142257+SameerNeupane03@users.noreply.github.com>
2025-04-17 07:30:27 +0545

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+public class HelloWorld {
+    public static void main(String[] args){
+        
+        //printing Hello World.
+        System.out.println("Hello World");
+
+        //sum of two numbers with 2 different variables.
+        int number1 = 3;
+        int number2 = 4;
+        
+        //adding two numbers and storing in add variable.
+        int add = number1 + number2;
+        System.out.println(add);
+    }
+}
